ac274d1de49251208123d34f9cf024402a9f56c1
nastyandreeva <36200697+nastyandreeva@users.noreply.github.com>
2018-12-28 16:31:18 +0300

    Update main.py

--- 1/oop/main.py
+++ 1/oop/main.py
@@ -1,50 +1,84 @@
-class Record():
-  def __init__(self, author, title):
-    self.__title = title
-    self.__author = author
+import re 
 
-  def show_comment(self, comment=object):
-    print(f"Заголовок коммента: {comment.title}, текст коммента: {comment.text}")
-
-# Разработка класса "комментарий" 
-class Comment():
-  def __init__(self, title, text):
-    self.__title = title
-    self.__text = text
-
-# Создание геттеров и сеттеров для классов «запись», «комментарий» поля title
-  @property
-  def title(self):
-    return self.__title
-    
-  @title.setter
-  def title(self, value):
-    self.__title = str(value)
-
-  @title.deleter
-  def title(self):
-    self.__title = None
-
-# Создание геттеров и сеттеров для классов «запись», «комментарий» поля text 
-  @property
-  def text(self):
-    return self.__text
-    
-  @text.setter
-  def text(self, value):
-    self.__text = str(value)
-
-  @text.deleter
-  def text(self):
-    self.__text = None
+class Person():
+  logger = []
   
-# Функция для вывода на печать информации, хранящийся в объектах.
-  def show(self):      
-    print("author: " + str(self.author))
-    print("title: " + str(self._title))
+  def __init__(self, user):
 
+    self.f_name = user["f_name"] 
+    self.s_name = user["s_name"]
+    self.birthyear = user["birthyear"] 
+    self.email = user["email"] 
+    self.country = user["country"]
+    self.city = user["city"]   
 
-# Создание объектов класса
-myR1 = Record('Andreeva', 'First record')
-myC1 = Comment('First comm', 'wow!')
-myR1.show_comment(comment=myC1)
+  @property
+  def birthyear(self):
+      return self._birthyear
+    
+  @birthyear.setter
+  def birthyear(self, value):
+    pattern1 =  r"^[1-2][0-9][0-9][0-9]"
+    prov1 = re.compile(pattern1)
+    self.logger.append("Cоздан паттерн для года рождения")
+
+    if (not (prov1.findall(str(value)))):
+      raise ValueError('Неверный формат года рождения')
+    else:
+      self.logger.append("Год прошел проверку")
+      self._birthyear = int(value)
+
+  @birthyear.deleter
+  def birthyear(self):
+      print('Year delete')
+      self._birthyear = None
+
+  @property
+  def email(self):
+      return self._email
+    
+  @email.setter
+  def email(self, value):
+    pattern2 = r"^[a-zA-Z0-9]{1,100}[@][a-z]{2,6}\.[a-z]{2,4}"
+    prov2 = re.compile(pattern2)
+    self.logger.append("Cоздан паттерн для email")
+
+    if (not (prov2.findall(str(value)))):
+      raise ValueError('Неверный формат email')
+    else:
+      self.logger.append("Email прошел проверку")
+      self._email = value
+
+  @email.deleter
+  def email(self):
+      print('Email delete')
+      self._email = None
+
+  def info(self):
+    self.participant_info = {
+      "f_name": self.f_name,
+      "s_name": self.s_name,
+      "email": self.email,
+      "birthyear": self.birthyear,
+      "country": self.country,
+      "city": self.city
+    }
+
+    return self.participant_info
+
+name = input("Введите имя:")
+surName = input("Введите фамилию:")
+year = input("Введите год рождения:")
+mail = input("Введите почту:")
+country = input("Введите страну:")
+town = input("Введите город:")
+
+new_dict={"f_name":name,"s_name":surName,"email":mail, "birthyear":year, "country":country, "city":town}
+
+new_user=Person(new_dict)
+
+with open('fileLog.txt', 'a') as file:
+  file.write(str(new_user.logger))
+
+with open('fileInfo.txt', 'a') as resultfile:
+  resultfile.write(str(new_dict.values())+"\n")
