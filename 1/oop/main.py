acc818114ea3404ac8dd02d398bbffa7e6078c28
nastyandreeva <36200697+nastyandreeva@users.noreply.github.com>
2018-12-28 15:05:57 +0300

    Create main.py

---
+++ 1/oop/main.py
@@ -0,0 +1,50 @@
+class Record():
+  def __init__(self, author, title):
+    self.__title = title
+    self.__author = author
+
+  def show_comment(self, comment=object):
+    print(f"Заголовок коммента: {comment.title}, текст коммента: {comment.text}")
+
+# Разработка класса "комментарий" 
+class Comment():
+  def __init__(self, title, text):
+    self.__title = title
+    self.__text = text
+
+# Создание геттеров и сеттеров для классов «запись», «комментарий» поля title
+  @property
+  def title(self):
+    return self.__title
+    
+  @title.setter
+  def title(self, value):
+    self.__title = str(value)
+
+  @title.deleter
+  def title(self):
+    self.__title = None
+
+# Создание геттеров и сеттеров для классов «запись», «комментарий» поля text 
+  @property
+  def text(self):
+    return self.__text
+    
+  @text.setter
+  def text(self, value):
+    self.__text = str(value)
+
+  @text.deleter
+  def text(self):
+    self.__text = None
+  
+# Функция для вывода на печать информации, хранящийся в объектах.
+  def show(self):      
+    print("author: " + str(self.author))
+    print("title: " + str(self._title))
+
+
+# Создание объектов класса
+myR1 = Record('Andreeva', 'First record')
+myC1 = Comment('First comm', 'wow!')
+myR1.show_comment(comment=myC1)
